d6c2cffa32e9fa6f95e6110aac2c178e10431995
Maximilian Bernkopf <maximilian.bernkopf@gmail.com>
2022-08-30 22:52:11 +0200

    more graphs

--- teslaquake/app.py
+++ teslaquake/app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 from datetime import date
 from typing import List
+import plotly.figure_factory as ff
+
+import sqlalchemy
+from sqlalchemy import create_engine
 
 from sqlmodel import Session, col, select
+from config import TESLAQUAKE_DATABASE_URL, DEBUG
 
-from database import engine
 from models import Event
 from config import TESLAQUAKE_COLUMNS
 from config import TESLAQUAKE_EVENTS
@@ -14,6 +17,8 @@ from config import START_DATE_ISO_STR
 
 STREAMLIT_MIN_DATE = date.fromisoformat(START_DATE_ISO_STR)
 STREAMLIT_MAX_DATE = date(2017,12,31)
+
+# APP CONFIG #####################################################
 
 st.set_page_config(
     page_title="Teslaquake", 
@@ -28,8 +33,43 @@ st.set_page_config(
         """
     }
 )
+
+# HELPERS #####################################################
+
+@st.experimental_singleton
+def init_connection():
+    engine = create_engine(TESLAQUAKE_DATABASE_URL, echo=DEBUG)
+    return engine
+
+engine = init_connection()
+
+
+@st.cache(hash_funcs={sqlalchemy.engine.Engine: id, sqlalchemy.orm.attributes.InstrumentedAttribute: lambda _: None})
+def get_filtered_events(
+    min_mag: float, 
+    max_mag: float, 
+    start_date: date, 
+    end_date: date, 
+    types_of_interest: List[str], 
+    engine) -> pd.DataFrame:
+    with Session(engine) as session:
+        statement = (
+            select(Event)
+            .where(col(Event.mag) >= min_mag, col(Event.mag) <= max_mag)
+            .where(col(Event.time) >= start_date, col(Event.time) <= end_date)
+            .where(col(Event.type).in_(types_of_interest))
+            )
+        events = session.exec(statement)
+        records = [e.dict() for e in events]
+        df = pd.DataFrame.from_records(records)
+        
+    return df
+
+# APP #####################################################
+
+
 st.title('⚡ Teslaquake ⚡')
-st.subheader('by Maximum Gainz Bernkopf')
+st.caption('by Maximum Gainz Bernkopf')
 
 
 with st.sidebar:
@@ -44,7 +84,7 @@ with st.sidebar:
         "Select a range of magnitudes", 
         min_value=0.0,
         max_value=10.0, 
-        value=(5.0, 10.0),
+        value=(0.0, 10.0),
         step=0.1
     )
 
@@ -55,42 +95,56 @@ with st.sidebar:
         format_func = lambda x: x.capitalize())
 
 
-def get_filtered_events(min_mag: float, max_mag: float, start_date: date, end_date: date, types_of_interest: List[str]) -> pd.DataFrame:
-    with Session(engine) as session:
-        statement = (
-            select(Event)
-            .where(col(Event.mag) >= min_mag, col(Event.mag) <= max_mag)
-            .where(col(Event.time) >= start_date, col(Event.time) <= end_date)
-            .where(col(Event.type).in_(types_of_interest))
-            )
-        events = session.exec(statement)
-        records = [e.dict() for e in events]
-        df = pd.DataFrame.from_records(records)
-
-    return df
-
-
 filtered_events_df = get_filtered_events(
     min_mag=min_mag, 
     max_mag=max_mag, 
     start_date=start_date, 
     end_date=end_date,
-    types_of_interest=types_of_interest
+    types_of_interest=types_of_interest,
+    engine=engine
     )
 
 if len(filtered_events_df.index) == 0:
     st.warning('There is no data available for your current selection....', icon="⚠️")
 
 else:
+
+    st.subheader(f"The biggest events for your current selection is:")
+    biggest_event = filtered_events_df[filtered_events_df['mag'] == filtered_events_df['mag'].max()]
+    st.dataframe(biggest_event.reset_index())
     filtered_events_df = filtered_events_df[TESLAQUAKE_COLUMNS]
+
+
+    st.subheader('Histogram by Magnitude')
+    fig = ff.create_distplot([filtered_events_df['mag']], ['Magnitude'], bin_size=.2, show_rug=False)
+    fig.update_xaxes(title_text='Magnitude')
+    st.plotly_chart(fig, use_container_width=True)
+
+
+    st.subheader('Most probable hour')
+    st.markdown("""Below we consider a density plot
+     of the hour for each of the buckets. 
+     Interpredation is open for discussion :)
+     """)
+    bins = [0, 1, 2, 3, 4, 5, 6, 10]
+    labels = ["0-1", "1-2", "2-3", "3-4", "4-5", "5-6", "6-10"]
+    filtered_events_df['bin'] = pd.cut(filtered_events_df['mag'], bins=bins, labels=labels)
+    filtered_events_df['hour'] = filtered_events_df['time'].dt.hour
+
+    tmp = []
+    for label in labels:
+        tmp.append(filtered_events_df[filtered_events_df['bin']==label]['hour'])
+
+    fig = ff.create_distplot(tmp, labels, show_hist=False, show_rug=False)
+    fig.update_xaxes(title_text='Hour')
+
+    st.plotly_chart(fig, use_container_width=True)
+
+
+    st.subheader('Map of all events')
+    st.map(filtered_events_df)
+    
 
     if st.checkbox('Show raw data'):
         st.subheader('Raw data')
         st.dataframe(filtered_events_df)
-
-    st.subheader('Histogram by Magnitude')
-    hist_values = np.histogram(filtered_events_df['mag'], bins=50, range=(0,10))[0]
-    st.bar_chart(hist_values)
-
-    st.subheader('Map of all events')
-    st.map(filtered_events_df)
